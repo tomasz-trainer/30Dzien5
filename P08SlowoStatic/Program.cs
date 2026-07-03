ca9c5719d282b058fb047ed64cf65d2b3fcea20c
tomasz <csharp@406-00>
2026-07-03 14:59:35 +0200

    q

---
+++ P08SlowoStatic/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P08SlowoStatic
+{
+    class Zawodnik
+    {
+        public string Imie { get; set; }
+
+        public static string Nazwisko { get; set; }
+
+        public string PrzedstawSie()
+        {
+            return $"Nazywam sie {Imie} {Nazwisko}";
+        }
+
+
+        public static void PowiedzKimJest()
+        {
+            Console.WriteLine($"Jestem {Nazwisko}");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zawodnik z = new Zawodnik();
+            z.Imie = "Jan";
+            //  z.Nazwisko = "Kowalski";
+            
+            Zawodnik z2 = new Zawodnik();
+            z2.Imie = "Adam";
+            //  z2.Nazwisko = "Nowak";
+
+            Zawodnik.Nazwisko = "Kowlaski"; // ustawienie nazwiska dla wszystkich zawodników
+
+            Console.WriteLine(z.PrzedstawSie());
+
+            Zawodnik.PowiedzKimJest();
+        }
+    }
+}
