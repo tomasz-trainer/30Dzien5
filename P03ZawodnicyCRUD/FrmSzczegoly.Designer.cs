7286a15df16a9c3afcb0dae340a7af1555f4d3b7
tomasz <csharp@406-00>
2026-07-03 11:57:23 +0200

    q

--- P03ZawodnicyCRUD/FrmSzczegoly.Designer.cs
+++ P03ZawodnicyCRUD/FrmSzczegoly.Designer.cs
@@ -41,6 +41,7 @@
             this.txtImie = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnZapisz = new System.Windows.Forms.Button();
+            this.btnUsun = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numWaga)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numWzrost)).BeginInit();
             this.SuspendLayout();
@@ -157,11 +158,23 @@
             this.btnZapisz.UseVisualStyleBackColor = true;
             this.btnZapisz.Click += new System.EventHandler(this.btnZapisz_Click);
             // 
+            // btnUsun
+            // 
+            this.btnUsun.Location = new System.Drawing.Point(12, 255);
+            this.btnUsun.Name = "btnUsun";
+            this.btnUsun.Size = new System.Drawing.Size(75, 23);
+            this.btnUsun.TabIndex = 28;
+            this.btnUsun.Text = "Usuń";
+            this.btnUsun.UseVisualStyleBackColor = true;
+            this.btnUsun.Visible = false;
+            this.btnUsun.Click += new System.EventHandler(this.btnUsun_Click);
+            // 
             // FrmSzczegoly
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(204, 293);
+            this.Controls.Add(this.btnUsun);
             this.Controls.Add(this.btnZapisz);
             this.Controls.Add(this.numWaga);
             this.Controls.Add(this.numWzrost);
@@ -199,5 +212,6 @@
         private System.Windows.Forms.TextBox txtImie;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnZapisz;
+        private System.Windows.Forms.Button btnUsun;
     }
 }
--- P03ZawodnicyCRUD/FrmSzczegoly.cs
+++ P03ZawodnicyCRUD/FrmSzczegoly.cs
@@ -23,21 +23,39 @@ namespace P03ZawodnicyCRUD
         TrybOkna trybOkna;
         Zawodnik zawodnik;
         FrmStartowy frmStartowy;
-        public FrmSzczegoly(TrybOkna trybOkna, ManagerZawodnikow mz, FrmStartowy frmStartowy)
+
+        /// <summary>
+        /// Tworzę formularz szczegółów zawodnika w trybie dodawania
+        /// </summary>
+        /// <param name="trybOkna">Określa tryb okna</param>
+        /// <param name="mz">Manager do zarządzania bazą</param>
+        /// <param name="frmStartowy">Referencja do formularza bazowego</param>
+        public FrmSzczegoly(TrybOkna trybOkna, ManagerZawodnikow mz, FrmStartowy frmStartowy) 
         {
+            InitializeComponent();
+            this.trybOkna = trybOkna;
             this.frmStartowy = frmStartowy;
             this.mz = mz;
-            InitializeComponent();
+
+            btnUsun.Visible = false; // ukrywam przycisk usuwania w trybie dodawania
+
         }
 
-
-        public FrmSzczegoly(TrybOkna trybOkna, ManagerZawodnikow mz, FrmStartowy frmStartowy, Zawodnik zawodnik)
+        /// <summary>
+        /// Tworzę formularz szczegółów zawodnika w trybie edycji
+        /// </summary>
+        /// <param name="trybOkna">Określa tryb okna</param>
+        /// <param name="mz">Manager do zarządzania bazą</param>
+        /// <param name="frmStartowy">Referencja do formularza bazowego</param>
+        /// <param name="zawodnik">Zawodnik do edycji</param>
+        public FrmSzczegoly(TrybOkna trybOkna, ManagerZawodnikow mz, FrmStartowy frmStartowy, Zawodnik zawodnik) : this(trybOkna, mz, frmStartowy)
         {
-            this.frmStartowy = frmStartowy;
-            this.mz = mz;
-            InitializeComponent();
+            //this.trybOkna = trybOkna;
+            //this.frmStartowy = frmStartowy;
+            //this.mz = mz;
+            //InitializeComponent();
 
-            this.trybOkna = trybOkna;
+
             this.zawodnik = zawodnik;
             txtImie.Text = zawodnik.Imie;
             txtNazwisko.Text = zawodnik.Nazwisko;
@@ -45,8 +63,9 @@ namespace P03ZawodnicyCRUD
             dtpDataUr.Value = zawodnik.DataUrodzenia;
             numWzrost.Value = zawodnik.Wzrost;
             numWaga.Value = zawodnik.Waga;
-        }
 
+            btnUsun.Visible = true; // pokazuję przycisk usuwania w trybie edycji
+        }
         private void btnZapisz_Click(object sender, EventArgs e)
         {
             if (trybOkna == TrybOkna.Dodawanie)
@@ -85,6 +104,11 @@ namespace P03ZawodnicyCRUD
             zawodnik.DataUrodzenia = dtpDataUr.Value;
             zawodnik.Wzrost = (int)numWzrost.Value;
             zawodnik.Waga = (int)numWaga.Value;
+        }
+
+        private void btnUsun_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
